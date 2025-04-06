fee12d06440661c5cb25d57732e75e84853c5e17
Mahdokht <ma5041i@gre.ac.uk>
2025-04-06 17:09:06 +0100

    Refactor: Added docstring and descriptions to main.py

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,11 @@
+"""
+Main script to run system log analysis using analysis_tools with defined input/ouput paths.
+"""
 import analysis_tools as at
 
-def main():
-    log_file = '../logs/sys.log'  
-    output_dir = 'results' 
+def main(): #Run analysis on log file
+    log_file = '../logs/sys.log'  #Path to input log file
+    output_dir = 'results' #Folder to save output
 
     at.analyze_log_file(log_file, output_dir)
 
