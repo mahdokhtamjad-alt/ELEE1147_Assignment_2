1351c8d1fe8f62e956f50b478edd16c449a45226
Mahdokht <ma5041i@gre.ac.uk>
2025-04-06 15:21:38 +0100

    Refactor: Added comments and descriptions to main.c

--- C/main.c
+++ C/main.c
@@ -1,33 +1,37 @@
+//Simulates a 2 hour system activity log
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h>
 #include "kernel.h"  
 
-
+//Converts a time value into ISO 8601 UTC format
 void get_iso8601_timestamp(char *buffer, size_t buffer_size, time_t current_time){ 
     struct tm *tm_info = gmtime(&current_time);  
     strftime(buffer, buffer_size, "%Y-%m-%dT%H:%M:%SZ", tm_info); 
 }
 
+//Main function that intializes hardware registers and writes readings to a log file
 int prime() {
-    srand((unsigned int)time(NULL)); 
-    init_registers(); 
+    srand((unsigned int)time(NULL)); //Seed random number generator
+    init_registers(); //Initialize registers
 
     time_t start_time = time(NULL);  
     time_t current_time = start_time;  
 
-    time_t simulated_end_time = current_time + 2 * 60 * 60;
+    time_t simulated_end_time = current_time + 2 * 60 * 60; //Simulate for 2 hours
 
     double time_increment = 1.36052; 
     char timestamp_buffer[32]; 
 
-    FILE *fp = fopen("../logs/sys.log", "w");
+    FILE *fp = fopen("../logs/sys.log", "w");   //Open log file
     if (fp == NULL) {
         perror("Failed to open file");  
         return 1;
     }
 
-    while (current_time < simulated_end_time){ 
+    //Loop for generating and logging readings every 1.36 seconds.
+    while (current_time < simulated_end_time){
+        //Creates timestamp for current time 
         get_iso8601_timestamp(timestamp_buffer, sizeof(timestamp_buffer), current_time); 
         
         unsigned int cpu_usage = read_cpu_usage();  
@@ -35,17 +39,18 @@ int prime() {
         unsigned int temperature = read_temperature();  
         unsigned int power_consumption = read_power_consumption();  
 
-        fprintf(fp, "%s - CPU: %u, MEM: %u, TEMP: %u, POWER: %u\n",
+        fprintf(fp, "%s - CPU: %u, MEM: %u, TEMP: %u, POWER: %u\n", // Write timestamp and generated readings to log file
                 timestamp_buffer,
                 cpu_usage,
                 memory_usage,
                 temperature,
                 power_consumption);
 
-        current_time += (time_t)time_increment;  
+        current_time += (time_t)time_increment;  //Increments current time and reinitializes registers
         init_registers();
     }  
 
+    // Close the file and confirm if successfull
     fclose(fp);  
     printf("System log generated successfully.\n");
     return 0;
