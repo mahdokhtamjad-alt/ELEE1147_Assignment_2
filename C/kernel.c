d368ea12c7f9be6b5681bf451d286dc45a780eef
Mahdokht <ma5041i@gre.ac.uk>
2025-04-06 14:03:10 +0100

    Refactor: Added descriptions for all functions

--- C/kernel.c
+++ C/kernel.c
@@ -1,21 +1,27 @@
+//Initialization
 #include "kernel.h"
 #include <stdlib.h>  
 #include <time.h>
+
 
 unsigned int CPU_REGISTER;
 unsigned int MEMORY_REGISTER;
 unsigned int TEMPERATURE_REGISTER;
 unsigned int POWER_REGISTER;
 
+//Write values to a register
 void write_register(unsigned int *reg, unsigned int value) 
 {
     *reg = value;
 
 }
+
+//Read a masked and shifted value from a register
 unsigned int read_register(unsigned int reg, unsigned int mask, unsigned int shift){ 
     return (reg & mask) >> shift; 
 }
 
+//Randomly initialize the registers
 void init_registers() {
 
         TEMPERATURE_REGISTER = (unsigned int)(rand() % (8890 - 3260 + 1) + 3260);  
@@ -25,19 +31,22 @@ void init_registers() {
 
 }
 
-
+//Return the CPU usage register
 unsigned int read_cpu_usage(){ 
     return CPU_REGISTER;  
 }
 
+//Return the memory usage register
 unsigned int read_memory_usage() {
     return MEMORY_REGISTER; 
 }
 
+//Return the temperature register after dividing by 100
 unsigned int read_temperture() {
     return TEMPERATURE_REGISTER / 100.0; 
 }
 
+//Return the power consumption register
 unsigned int read_power_consumption() {
     return POWER_REGISTER;
 }  
