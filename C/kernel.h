aaa5a5147db17d6632284f022cb1faa990c3c18b
Mahdokht <ma5041i@gre.ac.uk>
2025-04-06 15:30:35 +0100

    Fix: Corrected header and function declarations in kernel.h

--- C/kernel.h
+++ C/kernel.h
@@ -1,10 +1,15 @@
-#ifNdef KERNEL_H
+#ifndef KERNEL_H
+#define KERNEL_H
 
 
+void write_register(unsigned int *reg, unsigned int value);
+unsigned int read_register(unsigned int reg, unsigned int mask, unsigned int shift);
 
-unsigned int reeed_cpu_usg()
-        unsigned int reed_memry_usage()
-unsigned        int red_temperture()
-unsigned int rd_pwr _consump()
+void init_registers(void);
 
+unsigned int read_cpu_usg(void);
+unsigned int read_memry_usage(void);
+unsigned int read_temperture(void);
+unsigned int read_power_consumption(void);
 
+#endif
